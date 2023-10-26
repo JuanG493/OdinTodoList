2ff7fcaef3f407cf3e7cbb64fc7053ae22428745
JuanG493 <juanguarin493@gmail.com>
2023-10-26 08:13:00 +0200

    adding spaces

--- src/index.js
+++ src/index.js
@@ -1,55 +1,53 @@
-import './style.css';
-// eslint-disable-next-line import/no-cycle
-import { filter } from './priority.js';
+import "./style.css";
+import { filter } from "./priority.js";
 
-// eslint-disable-next-line import/no-cycle
 import {
   interfaceCheckList,
   interfaceNote,
   interfaceProyects,
   updateProjects,
-} from './interfacepj';
+} from "./interfacepj";
 
 function selector(target) {
   return document.querySelector(target);
 }
 
 const collectionToDos = {};
-const allProjects = ['Odin'];
-const formElm = selector('.formElm');
-const btnNew = selector('.btnNew');
-const btnCheckList = selector('.btnCheckL');
-const btnNote = selector('.btnNote');
-const btnNewProject = selector('.btnNewProject');
+const allProjects = ["Odin"];
+const formElm = selector(".formElm");
+const btnNew = selector(".btnNew");
+const btnCheckList = selector(".btnCheckL");
+const btnNote = selector(".btnNote");
+const btnNewProject = selector(".btnNewProject");
 
 function flow() {
   if (localStorage.length >= 1) {
     loadPjAndToDo();
   } else {
     //  load the Odin project as the dafault
-    updateProjects('Odin');
+    updateProjects("Odin");
   }
 
-  btnNew.addEventListener('click', () => {
+  btnNew.addEventListener("click", () => {
     renewForm(formElm);
     toggleClass([btnCheckList, btnNote]);
   });
 
-  btnCheckList.addEventListener('click', () => {
+  btnCheckList.addEventListener("click", () => {
     renewForm(formElm);
     mkGeneralElements();
     interfaceCheckList();
     toggleClass([btnNote, btnCheckList]);
   });
 
-  btnNote.addEventListener('click', () => {
+  btnNote.addEventListener("click", () => {
     renewForm(formElm);
     mkGeneralElements();
     interfaceNote();
     toggleClass([btnNote, btnCheckList]);
   });
 
-  btnNewProject.addEventListener('click', () => {
+  btnNewProject.addEventListener("click", () => {
     renewForm(formElm);
     interfaceProyects();
   });
@@ -59,23 +57,23 @@ flow();
 function toggleClass(elements) {
   // eslint-disable-next-line, guard-for-in, no-restricted-syntax
   for (const i in elements) {
-    elements[i].classList.toggle('show');
+    elements[i].classList.toggle("show");
   }
 }
 
 function renewForm(elem) {
-  const content = ' ';
+  const content = " ";
   elem.innerHTML = content;
 }
 
-function mkGeneralElements(vTitle = '', vData = '') {
-  const dayDate = new Date().toLocaleDateString().split('/');
+function mkGeneralElements(vTitle = "", vData = "") {
+  const dayDate = new Date().toLocaleDateString().split("/");
   formElm.innerHTML += `
     <label for='title'>Title</label>
     <input type='text' id='title' class='bsInfo inp' value='${vTitle}' required ></input>
     <label for='date'>date</label>
     <input type='date' id='date' class='bsInfo inp' 
-      min='${dayDate[2]}-${dayDate[0].padStart(2, '0')}-${dayDate[1]}' 
+      min='${dayDate[2]}-${dayDate[0].padStart(2, "0")}-${dayDate[1]}' 
       value='${vData}'></input>`;
 }
 
